4013d2abc2c72050cf2283d9053e7fc5834313a3
ezio4314 <98748220+ezio4314@users.noreply.github.com>
2022-07-06 22:13:48 +0300

    1

---
+++ учу JS/второй урок марафона/app.js
@@ -0,0 +1,15 @@
+const item = document.querySelector('.item')
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+
+function dragstart(event) {
+	console.log('drag start')
+	event.target.classList.add('hold')
+}
+
+function dragend(event) {
+	console.log('drag end')
+	event.target.classList.remove('hold')
+}
