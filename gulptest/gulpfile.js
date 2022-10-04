f14066c3def643eb114da1666e2dbf56495675e5
ezio4314 <98748220+ezio4314@users.noreply.github.com>
2022-10-04 22:21:09 +0300

    галп

--- gulptest/gulpfile.js
+++ gulptest/gulpfile.js
@@ -1,11 +1,23 @@
 import gulp from "gulp";
 import { path } from "./gulp/config/path.js";
 
+//Передаем значения в глобальную переменную
 global.app = {
 	path: path,
 	gulp: gulp
 }
 
+//Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
+import { reset } from "./gulp/tasks/reset.js";
 
-gulp.task('default', copy);
+//Наблюдетель за изменениями в файлах
+function watcher(){
+	gulp.watch(path.watch.files, copy)
+}
+
+//Построение сценариев выполнения задач
+const dev= gulp.series(reset, copy, watcher);
+
+//Выполнение сценария по умолчанию
+gulp.task('default', dev);
