1911d540f8f9c43998a8b9d0965e4c2e6d07752d
ezio4314 <98748220+ezio4314@users.noreply.github.com>
2022-10-05 22:11:30 +0300

    ебал галп

---
+++ gulptest/gulp/config/plugins.js
@@ -0,0 +1,6 @@
+import replace from "gulp-replace"; // поиск и замена
+
+// Экспортируем объект
+export const plugins = {
+	replace: replace
+}
--- gulptest/gulpfile.js
+++ gulptest/gulpfile.js
@@ -1,23 +1,29 @@
 import gulp from "gulp";
 import { path } from "./gulp/config/path.js";
+import { plugins } from "./gulp/config/plugins.js";
 
 //Передаем значения в глобальную переменную
 global.app = {
 	path: path,
-	gulp: gulp
+	gulp: gulp,
+	plugins: plugins
 }
 
 //Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
 
 //Наблюдетель за изменениями в файлах
 function watcher(){
 	gulp.watch(path.watch.files, copy)
+	gulp.watch(path.watch.html, html)
 }
 
+const mainTasks = gulp.parallel(copy, html);
+
 //Построение сценариев выполнения задач
-const dev= gulp.series(reset, copy, watcher);
+const dev= gulp.series(reset, mainTasks, watcher);
 
 //Выполнение сценария по умолчанию
 gulp.task('default', dev);
