b89f34a2badc961cee523479c4eb021174326643
ezio4314 <98748220+ezio4314@users.noreply.github.com>
2022-10-16 16:57:04 +0300

    галп все

--- gulptest/gulpfile.js
+++ gulptest/gulpfile.js
@@ -4,6 +4,8 @@ import { plugins } from "./gulp/config/plugins.js";
 
 //Передаем значения в глобальную переменную
 global.app = {
+	isBuild: process.argv.includes('--build'),
+	isDev: !process.argv.includes('--build'),
 	path: path,
 	gulp: gulp,
 	plugins: plugins
@@ -18,6 +20,8 @@ import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+import { svgSprive } from "./gulp/tasks/svgSprive.js";
+import { zip } from "./gulp/tasks/zip.js";
 
 //Наблюдетель за изменениями в файлах
 function watcher(){
@@ -28,6 +32,8 @@ function watcher(){
 	gulp.watch(path.watch.images, images);
 }
 
+export { svgSprive }
+
 //Последовательная обработка шрифтов
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
@@ -35,7 +41,14 @@ const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
 
 //Построение сценариев выполнения задач
-const dev= gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+const build = gulp.series(reset, mainTasks);
+const deployZIP = gulp.series(reset, mainTasks, zip);
+
+//Экспорт сценариев
+export { dev }
+export { build }
+export { deployZIP }
 
 //Выполнение сценария по умолчанию
 gulp.task('default', dev);
