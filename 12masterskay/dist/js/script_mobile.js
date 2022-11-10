9511af433b176b3e25ab7e786d7ed91bfb475298
ezio4314 <98748220+ezio4314@users.noreply.github.com>
2022-11-10 17:25:58 +0300

    1

--- 12masterskay/dist/js/script_mobile.js
+++ 12masterskay/dist/js/script_mobile.js
@@ -38,8 +38,37 @@ if (iconBurger){
 	});
 }
 
-// if (iconBurger.classList.contains('active')) {
-// 	document.body.classList.remove('lock');
-// 	iconBurger.classList.remove('active');
-// 	headerNav.classList.remove('active');
-//}
+if (iconBurger.classList.contains('active')) {
+	document.body.classList.remove('lock');
+	iconBurger.classList.remove('active');
+	headerNav.classList.remove('active');
+}
+
+// Плавная прокрутка при клике
+
+const headerLinks = document.querySelectorAll('.header_link[data-goto]');
+if (headerLinks.length > 0) {
+	headerLinks.forEach(headerLink => {
+		headerLink.addEventListener("click", onHeaderLinkClick);
+	});
+
+	function onHeaderLinkClick(e) {
+		const headerLink = e.target;
+		if (headerLink.dataset.goto && document.querySelector(headerLink.dataset.goto)){
+			const gotoBlock = document.querySelector(headerLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect(). top + pageYOffset - document.querySelector('header').offsetHeight;
+		
+			if (iconBurger.classList.contains('active')) {
+				document.body.classList.remove('lock');
+				iconBurger.classList.remove('active');
+				headerNav.classList.remove('active');
+			}
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: "smooth"
+			});
+			e.preventDefault();
+		}
+	}
+}
